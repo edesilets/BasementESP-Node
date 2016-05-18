8d084c7a864c35376e5f64a0a00d31bfe9962e64
Ethan <erdesi90562@gmail.com>
2016-05-18 12:25:04 -0400

    Add multiple DS18b20 Sensors.
    Caution hacked the DHT to return data instead of nan. See Code comment.

--- basementESP8266/basementESP8266.ino
+++ basementESP8266/basementESP8266.ino
@@ -1,15 +1,27 @@
 /************** INCLUDE!!! **************/
 #include "config.h"
+/******** MQTT library Requirements *****/
 #include "ESP8266WiFi.h"
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+/******** DHT library Requirements (thermometer, humidity) *****/
 #include "DHT.h"
+/******** DS18B20 library Requirements *****/
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 /************** Define!!!! See config.h **************/
 // SEE config.h to set your wifi password.
 // SEE config.h to set your wifi ssid.
 // SEE config.h to set your MQTT server, username and passwordpassword.
 
 DHT dht(DHTPIN, DHTTYPE); // GPIOpins, sensor type These are defined in config.h.
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+byte ThermometerInlet[] = { 0x28, 0x81, 0x93, 0xDE, 0x06, 0x00, 0x00, 0xFF };
+byte ThermometerOutlet[] = { 0x28, 0xBC, 0x97, 0xDE, 0x06, 0x00, 0x00, 0x47 };
 
 /************ Setup MQTT information ******************/
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
@@ -25,8 +37,10 @@ const char MQTT_PASSWORD[] PROGMEM = PASSWORD;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, 1883, MQTT_USERNAME, MQTT_PASSWORD);
 
 /****************************** Feeds ***************************************/
-Adafruit_MQTT_Publish temperatureRoom = Adafruit_MQTT_Publish(&mqtt, "/home/livingroom/thermostat/temperature/livingroom");
-Adafruit_MQTT_Publish humidityRoom = Adafruit_MQTT_Publish(&mqtt, "/home/livingroom/thermostat/humidity/livingroom");
+Adafruit_MQTT_Publish temperatureRoom = Adafruit_MQTT_Publish(&mqtt, "/home/basement/thermostat/temperature/basement");
+Adafruit_MQTT_Publish humidityRoom = Adafruit_MQTT_Publish(&mqtt, "/home/basement/thermostat/humidity/basement");
+Adafruit_MQTT_Publish temperatureOutletPipe = Adafruit_MQTT_Publish(&mqtt, "/home/basement/boiler/temperature/outletpipe");
+Adafruit_MQTT_Publish temperatureInletPipe = Adafruit_MQTT_Publish(&mqtt, "/home/basement/boiler/temperature/inletpipe");
 /*************************** Sketch Code ************************************/
 
 // Bug workaround for Arduino 1.6.6, it seems to need a function declaration
@@ -40,7 +54,7 @@ void setup() {
   Serial.println(F("Adafruit MQTT demo"));
 
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
@@ -56,6 +70,10 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
   dht.begin();
+  /********************** Tempature ******************************/
+  sensors.begin();
+  sensors.setResolution(ThermometerInlet, TEMPERATURE_PRECISION);
+  sensors.setResolution(ThermometerOutlet, TEMPERATURE_PRECISION);
 }
 
 void loop() {
@@ -67,9 +85,31 @@ void loop() {
   if(! mqtt.ping()) {
     mqtt.disconnect();
   }
-  delay(2000);
-  humidityRoom.publish(dht.readHumidity());
-  temperatureRoom.publish(dht.readTemperature(true));
+
+  float roomHumidity = dht.readHumidity();
+  float roomTemperature = dht.readTemperature(true);
+
+  // These If statements are a hack. Something weird is happening with the dht
+  // and the DS18B20. This makes sure that data is returned insted of nan.
+  if (!isnan(roomHumidity)) {
+    humidityRoom.publish(roomHumidity);
+  }
+
+  if (!isnan(roomTemperature)) {
+    temperatureRoom.publish(roomTemperature);
+  }
+
+  sensors.requestTemperatures();
+  temperatureInletPipe.publish(printTemperature(ThermometerInlet));
+  temperatureOutletPipe.publish(printTemperature(ThermometerOutlet));
+}
+
+//function to print the temperature for a device
+float printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  float tempF = DallasTemperature::toFahrenheit(tempC);
+  return tempF;
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -86,7 +126,7 @@ void MQTT_connect() {
 
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
+       Serial.println("Retrying MQTT connection in 2 seconds...");
        mqtt.disconnect();
        delay(2000);  // wait 2 seconds
   }
